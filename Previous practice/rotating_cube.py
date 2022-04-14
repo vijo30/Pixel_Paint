dbd2785a782132ea56afe2407026df2fea8597a4
vijo30 <101999975+vijo30@users.noreply.github.com>
2022-04-14 11:29:10 -0400

    .
    .

---
+++ Previous practice/rotating_cube.py
@@ -0,0 +1,132 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+import numpy as np
+import pyrr
+
+vertex_src = """
+# version 330
+
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_color;
+
+uniform mat4 rotation;
+
+in vec3 a_position;
+in vec3 a_color;
+out vec3 v_color;
+
+void main()
+{
+    gl_Position = rotation * vec4(a_position, 1.0);
+    v_color = a_color;
+}
+"""
+
+fragment_src = """
+# version 330
+
+in vec3 v_color;
+out vec4 out_color;
+
+void main()
+{
+    out_color = vec4(v_color, 1.0);
+}
+"""
+
+# resize window
+def window_resize(window, width, height):
+  glViewport(0, 0, width, height)
+
+
+# initialize glfw
+if not glfw.init():
+  raise Exception("glfw can not be initialized!")
+
+# create window
+window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
+
+# check if window was created
+if not window:
+  glfw.terminate()
+  raise Exception("glfw window can not be created!")
+
+# set window's position
+glfw.set_window_pos(window, 400, 200)
+
+# resize callback
+glfw.set_window_size_callback(window, window_resize)
+
+# make the context current
+glfw.make_context_current(window)
+
+
+# vertices and color of the cube
+
+vertices = [-0.5, -0.5,  0.5,  1.0,  0.0,  0.0,
+             0.5, -0.5,  0.5,  0.0,  1.0,  0.0,
+             0.5,  0.5,  0.5,  0.0,  0.0,  1.0,
+            -0.5,  0.5,  0.5,  1.0,  1.0,  1.0,
+             
+            -0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+             0.5, -0.5, -0.5,  0.0,  1.0,  0.0,
+             0.5,  0.5, -0.5,  0.0,  0.0,  1.0,
+            -0.5,  0.5, -0.5,  1.0,  1.0,  1.0,
+             ]
+
+indices = [0, 1, 2, 2, 3, 0, # each row specifies a face of the cube
+           4, 5, 6, 6, 7, 4,
+           4, 5, 1, 1, 0, 4,
+           6, 7, 3, 3, 2, 6,
+           5, 6, 2, 2, 1, 5,
+           7, 4, 0, 0, 3, 7]
+             
+# convert list to array
+
+vertices = np.array(vertices, dtype=np.float32)
+indices = np.array(indices, dtype=np.uint32)
+
+# pipeline
+shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
+
+# vertex buffer object
+VBO = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, VBO)
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+
+EBO = glGenBuffers(1)
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
+
+
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
+glUseProgram(shader)
+glClearColor(0, 0.1, 0.1, 1) # window colors
+glEnable(GL_DEPTH_TEST)
+rotation_loc = glGetUniformLocation(shader, "rotation")
+
+# the main loop
+while not glfw.window_should_close(window):
+  glfw.poll_events()
+  
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # change window color
+  
+  rot_x = pyrr.Matrix44.from_x_rotation(0.5 * glfw.get_time())
+  rot_y = pyrr.Matrix44.from_y_rotation(0.8 * glfw.get_time())
+  
+  #glUniformMatrix4fv(rotation_loc, 1, GL_FALSE, rot_x * rot_y)
+  #glUniformMatrix4fv(rotation_loc, 1, GL_FALSE, rot_x @ rot_y)
+  glUniformMatrix4fv(rotation_loc, 1, GL_FALSE, pyrr.matrix44.multiply(rot_x, rot_y))
+   
+  glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+  
+  glfw.swap_buffers(window)
+  
+# terminate glfw
+glfw.terminate()
